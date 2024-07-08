78c313569a6668db2ca0ae87f02f4e7de116100e
Sardar219 <sardarhossaini2022@gmail.com>
2024-07-08 09:02:22 +0430

    add random color function for background

--- script.js
+++ script.js
@@ -60,4 +60,47 @@ function loadTrack(track_index) {
   curr_track.src = music_list[track_index].music;
   curr_track.load();
   track_art.style.backgroundImage = "url(" + music_list[track_index].img + ")";
+  track_name.textContent = music_list[track_index].name;
+  track_artist.textContent = music_list[track_index].artist;
+  now_playing.textContent =
+    "Playing music" + (track_index + 1) + " of " + music_list.length;
+
+  updateTimer = setInterval(setUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+}
+
+function random_bg_color() {
+  let hex = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+  ];
+  let a;
+  function populate(a) {
+    for (let i = 0; i < 6; i++) {
+      let x = Math.round(Math.random() * 14);
+      let y = hex[x];
+      a += y;
+    }
+    return a;
+  }
+  let Color1 = populate("#");
+  let Color2 = populate("#");
+  var angle = "to right";
+
+  let gradient = "linear-gradient(" + angle + "," + Color1 + "," + Color2 + ")";
+  document.body.style.background = gradient;
 }
